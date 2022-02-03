8ab45ea0613a71ec8156568c737b08debac393ea
DESKTOP-KVCQHCD\SSAFY <dawon950@gmail.com>
2022-02-03 11:35:45 +0900

    Chore : Import 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Container from "@mui/material/Container";
-import NavBar from "./pages/NavBar";
-import Home from ".//Home";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HospitalReservation from "./pages/hospital/HospitalReservation";
-import HospitalSearch from "./pages/hospital/HospitalSearch";
-import Notice from "./pages/Notice";
+import HospitalReservation from "./components/hospital/HospitalReservation";
+import HospitalSearch from "./components/hospital/HospitalSearch";
+import Notice from "./components/Notice";
 // import UserLoginModal from "./components/UserLoginModal";
 
 function App() {
