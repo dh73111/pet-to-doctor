93c2747629f802c39e511e0c8e911504d2471ffc
junaem <junh9547@gmail.com>
2022-02-05 01:08:11 +0900

    feat : 회원가입시 이메일 인증 추가, 인증 안 된 사용자 로그인 불가

--- backend/src/main/java/com/ssafy/pettodoctor/api/service/SendMailService.java
+++ backend/src/main/java/com/ssafy/pettodoctor/api/service/SendMailService.java
@@ -2,6 +2,8 @@ package com.ssafy.pettodoctor.api.service;
 
 
 import com.ssafy.pettodoctor.api.domain.User;
+import com.ssafy.pettodoctor.api.domain.UserCertification;
+import com.ssafy.pettodoctor.api.repository.UserCertificationRepository;
 import com.ssafy.pettodoctor.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,31 +25,34 @@ import java.util.Random;
 public class SendMailService {
 
     private final UserRepository userRepository;
+    private final UserCertificationRepository userCertificationRepository;
 
     final private static String myMail = "jh.javamail@gmail.com";
     final private static String password = "a789a789";
+    final private static String fromMail = "jh.javamail@gmail.com";
+    final private static String fromName = "Pet-To-Doctor";
+
+    private String getRandomKey() {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        return random.ints(leftLimit,rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
 
     @Transactional
     public void sendPassword(String userEmail) {
 
         User user = userRepository.findByEmail(userEmail);
 
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 8;
-        Random random = new Random();
-
-        String generatedPassword = random.ints(leftLimit,rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-
-        user.setPassword(generatedPassword);
+        user.setPassword(getRandomKey());
 
         String subject = "펫투닥터 비밀번호 재발급";
-        String fromMail = "jh.javamail@gmail.com";
-        String fromName = "Pet-To-Doctor";
 
         StringBuffer content = new StringBuffer();
         content.append("<h1>펫투닥터에서 재발급된 비밀번호입니다.</h1>\n");
@@ -63,7 +68,6 @@ public class SendMailService {
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-//        props.put("mail.smtp.atarttls.enable", "true");
 
         Session mailSession = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
                 protected PasswordAuthentication getPasswordAuthentication() {
@@ -80,13 +84,60 @@ public class SendMailService {
             message.setContent(content.toString(), "text/html;charset=UTF-8");
             message.setSentDate(new Date());
 
-//            Transport t = mailSession.getTransport("smtp");
-//            t.connect(myMail, password);
-//            t.sendMessage(message, message.getAllRecipients());
-//            t.close();
             Transport.send(message);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    @Transactional
+    public void sendCertification(String userEmail) {
+
+        User user = userRepository.findByEmail(userEmail);
+        StringBuilder certKey = new StringBuilder();
+        certKey.append(getRandomKey()).append(userEmail);
+
+        UserCertification uc = new UserCertification();
+        uc.setUser(user);
+        uc.setCertificationKey(certKey.toString());
+
+        userCertificationRepository.save(uc);
+
+        String subject = "펫투닥터 인증 이메일입니다.";
+
+        StringBuffer content = new StringBuffer();
+        content.append("<h1> 아래의 링크를 클릭해 계정 인증을 완료해주세요. </h1>\n");
+        content.append("<br>");
+        content.append("<a href=\"http://localhost:8080/api/user/certification/").append(uc.getCertificationKey()).append("\">대충 로그인 링크</a>");
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", 465);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        Session mailSession = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myMail, password);
+            }
+        });
+
+        try {
+            MimeMessage message = new MimeMessage(mailSession);
+
+            message.setFrom(new InternetAddress(fromMail, MimeUtility.encodeText(fromName, "UTF-8", "B")));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(userEmail));
+            message.setSubject(subject);
+            message.setContent(content.toString(), "text/html;charset=UTF-8");
+            message.setSentDate(new Date());
+
+            Transport.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
