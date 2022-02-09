de4c628af2a63ffb9bdffecc67a28723624b72bc
junaem <junh9547@gmail.com>
2022-02-09 22:06:41 +0900

    modify : 패스워드 변경 시 기존 pw 아닌 새 pw 두번 확인

--- backend/src/main/java/com/ssafy/pettodoctor/api/request/UserPasswordChangeReq.java
+++ backend/src/main/java/com/ssafy/pettodoctor/api/request/UserPasswordChangeReq.java
@@ -6,6 +6,6 @@ import lombok.Setter;
 @Getter @Setter
 public class UserPasswordChangeReq {
     private String password;
-    private String passwordConf;
     private String newPassword;
+    private String newPasswordConf;
 }
