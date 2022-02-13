7a1af98c7638b2c5131790d520a3501dd9ff4f17
junaem <junh9547@gmail.com>
2022-02-13 20:13:28 +0900

    feat : revewRes에 병원명, 주소 추가

--- backend/src/main/java/com/ssafy/pettodoctor/api/response/ReviewRes.java
+++ backend/src/main/java/com/ssafy/pettodoctor/api/response/ReviewRes.java
@@ -1,5 +1,6 @@
 package com.ssafy.pettodoctor.api.response;
 
+import com.ssafy.pettodoctor.api.domain.Address;
 import com.ssafy.pettodoctor.api.domain.Review;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,29 +17,35 @@ public class ReviewRes {
     private String content;
     private Integer rate;
     private LocalDateTime createTime;
-    private String username;
+    private String userName;
+    private String hospitalName;
+    private Address hospitalAddress;
 
     public ReviewRes() {
     }
 
-    public ReviewRes(Long id, Long userId, Long hospitalId, String content, Integer rate, LocalDateTime createTime, String username) {
+    public ReviewRes(Long id, Long userId, Long hospitalId, String content,
+                     Integer rate, LocalDateTime createTime, String username,
+                     String hospitalName, Address hospitalAddress) {
         this.id = id;
         this.userId = userId;
         this.hospitalId = hospitalId;
         this.content = content;
         this.rate = rate;
         this.createTime = createTime;
-        this.username = username;
+        this.userName = username;
+        this.hospitalName = hospitalName;
+        this.hospitalAddress = hospitalAddress;
     }
 
     public static List<ReviewRes> convertToResList(List<Review> reviews){
         List<ReviewRes> result = new ArrayList<>();
         for(Review r : reviews){
-            ReviewRes rr = new ReviewRes(r.getId(), r.getUser().getId()
-                    , r.getHospital().getId(), r.getContent(), r.getRate(), r.getCreateTime(), r.getUser().getName());
+            ReviewRes rr = new ReviewRes(r.getId(), r.getUser().getId(),
+                    r.getHospital().getId(), r.getContent(), r.getRate(), r.getCreateTime(),
+                    r.getUser().getName(), r.getHospital().getName(), r.getHospital().getAddress());
             result.add(rr);
         }
-
         return result;
     }
 }
