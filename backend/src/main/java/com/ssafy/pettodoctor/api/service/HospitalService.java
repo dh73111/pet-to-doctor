de45357f8d1e127b6abbf48580bfa60535bc10ab
junaem <junh9547@gmail.com>
2022-02-13 16:04:03 +0900

    feat : 병원 프로필 이미지 추가

--- backend/src/main/java/com/ssafy/pettodoctor/api/service/HospitalService.java
+++ backend/src/main/java/com/ssafy/pettodoctor/api/service/HospitalService.java
@@ -3,9 +3,17 @@ package com.ssafy.pettodoctor.api.service;
 import com.ssafy.pettodoctor.api.domain.Hospital;
 import com.ssafy.pettodoctor.api.repository.HospitalRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Service
@@ -13,6 +21,9 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class HospitalService {
     private final HospitalRepository hospitalRepository;
+
+    @Value("${profileImg.path}")
+    private String uploadFolder;
 
     public Hospital findById(Long id){
         return hospitalRepository.findById(id);
@@ -26,7 +37,28 @@ public class HospitalService {
         return hospitalRepository.findByName(name);
     }
 
-    public List<Hospital> findByDongName(String dongName){
-        return hospitalRepository.findByDongName(dongName);
+    public List<Hospital> findByDongName(String dongName){ return hospitalRepository.findByDongName(dongName); }
+
+    public void updateProfile(Long hospitalId, MultipartFile multipartFile) {
+        Hospital hospital = hospitalRepository.findById(hospitalId);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        String imageFileName = "hospital_" + hospital.getId() + "." + FilenameUtils.getExtension(multipartFile.getOriginalFilename());
+        Path imageFilePath = Paths.get(uploadFolder + imageFileName);
+
+        if (multipartFile.getSize() != 0){
+            try{
+                if (hospital.getProfileImgUrl() != null) {
+                    File file = new File(uploadFolder + hospital.getProfileImgUrl());
+                    file.delete();
+                }
+                Files.write(imageFilePath, multipartFile.getBytes());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            hospital.setProfileImgUrl(imageFileName);
+        }
+
     }
 }
